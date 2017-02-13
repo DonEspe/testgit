69715629efb92d839724418af89d5177e18bf695
BuildTools <unconfigured@null.spigotmc.org>
2017-02-13 16:08:40 -0600

    Initial Commit

---
+++ testgit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Don Espe on 2/13/17.
+//  Copyright © 2017 Ducky Planet LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
